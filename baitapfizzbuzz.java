6dc454e1e340676774b11551d3fbc8d362c7d66d
rin3101 <linh1526816@gmail.com>
2024-10-11 12:21:15 +0900

    Add files via upload

---
+++ baitapfizzbuzz.java
@@ -0,0 +1,14 @@
+public class baitapfizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            if (i % 3==0) {if (i % 5==0) {
+                System.out.println("fizzBuzz :"+i);
+            }else{
+                System.out.println("fizz :"+i);
+            }
+            System.out.println("buzz :"+i);     
+            }    
+            }   
+        }
+    }
+
